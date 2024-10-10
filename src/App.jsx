6c6452537d15e1921067850ad81618e33ad8b666
asifpashraf <asifpashraf@gmail.com>
2024-10-10 13:19:30 +0530

    updation

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 function App() {
   return (
     <div>
-      
-      
+      welcome
     </div>
   )
 }
--- src/layout-component/head.jsx
+++ src/layout-component/head.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function head() {
   return (
-    <div>head</div>
+    <div></div>
   )
 }
