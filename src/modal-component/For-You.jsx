e9c3e8619490151f42a61e5df4f0e550ba47873a
Hashim Poroli <porolihashim36@gmail.com>
2024-10-17 11:54:13 +0530

    new

---
+++ src/modal-component/For-You.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Foryouimage from '/src/assets/Foryou-image.svg'
+
+const ForYou = () => {
+  return (
+    <div>
+      <div className='foryou-box'>
+        <img className='foryou-Image' src={Foryouimage} alt="" />
+        <span className='foryou-text' >Nothing here Yet</span>
+        <span className='foryou-text2'>Come back for notifications on events, streams, and more.</span>
+        
+      </div>
+
+    </div>
+  )
+}
+
+export default ForYou
