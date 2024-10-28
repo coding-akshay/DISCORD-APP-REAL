727b9dd95f8fc659cf16ccad573fbdb8c005fc54
unknown <asifpashraf@gmail.com>
2024-10-28 10:44:39 +0530

    ci

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import Sidebar from './layout-component/Sidebar'
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
-import Dm from './layout-component/dm';
+
 import RouterComponent from './routes/RouterComponent';
 
 function App() {
   return (
     <Router>
     <div className='main'>
-      {/* <Dm /> */}
       <RouterComponent />
     </div>
   </Router>
